4a0e80fc90883d4e678c2405a8b3c7486e874b99
daniil-torubara <dabarara@bk.ru>
2023-08-06 20:55:53 +0600

    module 3 task 4

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c, h, a, r;
+        c = 99;
+        h = 104;
+        a = 97;
+        r = 114;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
